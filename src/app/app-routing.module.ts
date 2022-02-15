a1df429a6e73850155026c4769ddd2ab8ae07dd2
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-02-15 11:40:41 +0200

    add header and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamesComponent } from './games/games.component';
+import { LibraryComponent } from './library/library.component';
+import { FriendsComponent } from './friends/friends.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/games', pathMatch: 'full' },
+  { path: 'games', component: GamesComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'friends', component: FriendsComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GamesComponent } from './games/games.component';
+import { LibraryComponent } from './library/library.component';
+import { FriendsComponent } from './friends/friends.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GamesComponent,
+    LibraryComponent,
+    FriendsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
