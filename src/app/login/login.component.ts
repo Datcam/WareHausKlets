ae63d36ad2d21cc35965a09da74b09e2e98f8d21
Mariia <mariia.markiv.knm.2020@lpnu.ua>
2022-02-16 20:41:33 +0200

    fixed form component binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
