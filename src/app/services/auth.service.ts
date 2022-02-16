0bafd8fedc1e03ffcb6dd0778f5d58468ff54b91
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-02-16 12:28:04 +0200

    add login routing functionality

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+export class AuthService {
+  isLoggedIn = false;
+
+  isAuth() {
+    return this.isLoggedIn
+  }
+
+  logIn() {
+    this.isLoggedIn = true;
+  }
+
+  logOut() {
+    this.isLoggedIn = false;
+  }
+}
