3c8f1ad94146128a5aea9116e5b79eb8363b2197
Iryna Korolova <korolova.lviv@gmail.com>
2022-02-16 16:52:02 +0200

    filter and search logic added

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -16,6 +16,8 @@ interface Game {
 })
 export class GamesComponent implements OnInit {
   tags: string[] = [];
+  maxPrice = 0;
+  minPrice = 0;
   data: Game[] = [
     {
       id: 1,
@@ -72,8 +74,76 @@ export class GamesComponent implements OnInit {
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, omnis libero? Molestias, laboriosam nobis? Quae harum nemo aut nesciunt distinctio aliquid accusamus quis possimus at iure vitae vero amet sunt quidem impedit consequuntur error ea, nihil soluta!',
     },
   ];
+  filteredData: Game[] = [];
+  priceLimit = 0;
+  filterTags: string[] = [];
+  searchValue = '';
 
   ngOnInit(): void {
+    this.setFilteredData(this.data);
+    this.mapTags();
+    this.setMaxPrice();
+    this.setMinPrice();
+  }
 
+  mapTags(): void {
+    this.tags = this.data.map((game) => game.genre);
+  }
+
+  setMaxPrice(): void {
+    this.maxPrice = Math.max(...this.data.map((game) => game.price));
+  }
+
+  setMinPrice(): void {
+    this.minPrice = Math.min(...this.data.map((game) => game.price));
+  }
+
+  setFilteredData(filteredData: Game[]) {
+    this.filteredData = filteredData;
+  }
+
+  filterGames(event: any) {
+    event.preventDefault();
+
+    this.priceLimit = Number(event.target.price.value);
+
+    this.filterTags = Array.from(event.target.tag)
+      .filter((input: any) => input.checked)
+      .map((input: any) => input.value);
+
+    let games = this.data;
+    if (this.searchValue) {
+      games = this.search(games);
+    }
+
+    const filteredGames = this.filter(games);
+    this.setFilteredData(filteredGames);
+  }
+
+  searchGames(event: any) {
+    event.preventDefault();
+
+    this.searchValue = event.target.search.value.trim().toLowerCase();
+
+    let games = this.data;
+    if (this.priceLimit || this.filterTags.length) {
+      games = this.filter(games);
+    }
+
+    const foundGames = this.search(games);
+    this.setFilteredData(foundGames);
+  }
+
+  search(games: Game[]) {
+    return games.filter((game) => game.name.toLowerCase().includes(this.searchValue));
+  }
+
+  filter(games: Game[]) {
+    return games.filter((game) => {
+      return (
+        game.price <= this.priceLimit &&
+        (this.filterTags.length ? this.filterTags.includes(game.genre) : true)
+      );
+    });
   }
 }
