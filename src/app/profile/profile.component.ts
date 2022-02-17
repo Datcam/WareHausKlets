c9d9bb4cfc4c99b706ea5c24d1aeea5efa7afe38
Mariia <mariia.markiv.knm.2020@lpnu.ua>
2022-02-17 14:20:45 +0200

    login change(improve check)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginComponent } from '../login/login.component';
+import { AuthService } from '../services/auth.service';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
+@Injectable()
+
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  form!: FormGroup
+  enteredEmail! : string
+  constructor(private auth: AuthService) { }
+  
+  ngAfterContetnChecked(){
+    
   }
+  ngOnInit(): void {
+    this.enteredEmail = this.auth.email;
+  console.log(this.enteredEmail)
+  this.form = new FormGroup({
+    name: new FormControl(null,[Validators.required, Validators.minLength(5)]),
+    
+    age:new FormControl(null,[Validators.required])})
 
+  }
+  onSubmit(){
+
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,14 @@
 export class AuthService {
   isLoggedIn = false;
-
+  email!: string
+  getEmail(){
+    return this.email
+  }
+  setEmail(email:string){
+    this.email = email
+  }
   isAuth() {
+
     return this.isLoggedIn
   }
 
